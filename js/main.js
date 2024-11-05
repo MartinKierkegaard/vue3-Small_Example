c4c4f03d4a43f170c24d36d0d64976d812f53e13
Martin Kierkegaard <MARK@zealand.dk>
2024-11-05 11:24:12 +0100

    tilføjet v-on v-model eksempel

--- js/main.js
+++ js/main.js
@@ -2,10 +2,16 @@ const app = Vue.createApp({
     data() {
         return {
             intro: 'Welcome to my Vue template',
+            liste:[],
+            navn:null,
+            id:null,
+
         }
     },
     methods: {
-        myMethod(){
+        addMethod(){
+            this.liste.push({navn:this.navn,id:this.id})
+
 
         },
     },
